e4b1e8e945391159b3af05b1ccfc77361ea82bb7
José Antonio Cuello Principal <yopli2000@gmail.com>
2023-08-18 19:57:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Aplicación Web: BiblioAPP
+Open source web application software to manage a sample of books that can be rented for a time defined by the user.
+These users can also leave reviews about the books they rent for other users.
+
+## Requirements
+This application is based on the WebApp application.
---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+use WebApp\Core\App\Router;
+use Whoops\Handler\JsonResponseHandler;
+use Whoops\Handler\PlainTextHandler;
+use Whoops\Handler\PrettyPageHandler;
+use Whoops\Run;
+
+const APP_FOLDER = __DIR__;
+
+// load required files
+require_once  APP_FOLDER . '/vendor/autoload.php';
+require_once APP_FOLDER . '/config.php';
+
+// disable execution time and connection abort
+@set_time_limit(0);
+ignore_user_abort(true);
+
+// set timezone
+date_default_timezone_set(APP_TIMEZONE);
+
+// Register error handler
+if (APP_DEBUG) {
+    $whoops = new Run();
+    $whoops->prependHandler(new PlainTextHandler());
+    $whoops->prependHandler(new JsonResponseHandler());
+    $whoops->prependHandler(new PrettyPageHandler());
+    $whoops->register();
+}
+
+// Run application. Can be a download file or a web page.
+// First try to get the file to download, if not, run the web page.
+$router = new Router();
+if (false === $router->getFile()) {
+    $app = $router->getApp();
+    $app->connect();        // Connect to the database, cache, etc.
+    $app->run();            // Executes App logic
+    $app->render();         // Render web page
+    $app->close();          // Disconnect from everything
+}
